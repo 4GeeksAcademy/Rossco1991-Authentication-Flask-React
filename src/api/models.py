08166b59e81a15acb6a0ab38832f08bb56145e8d
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-08-10 00:55:40 +0000

    Completed Project

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -16,4 +17,18 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Private(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    email_show = db.relationship('User', uselist=False, backref=db.backref('User', uselist=True))
+
+    def __repr__(self):
+        return f'<Private {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email_show": self.email_show.serialize(True)
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import (create_access_token, get_jwt_identity, jwt_required)
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['GET','POST'])
+def sign_ups():
+    data = request.json
+    user = User.query.filter_by(email=data.get('email', None)).first()
+    if user:
+        return jsonify(message="User already exists"), 400
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return '', 204
+
+@api.route('/login', methods=['GET','POST'])
+def login():
+      data = request.json
+      user = User.query.filter_by(email=data.get('email', None)).first()
+
+      if not user:
+           return jsonify(message="Invalid username"), 401
+      if user.password != data.get("password", None):
+           return jsonify(message="Invalid password"), 401
+      
+      token= create_access_token(user.email)
+      return jsonify(token=token)
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def secret_tunnel():
+        user = User.query.filter_by('email').first()
+        return jsonify(user)
+
